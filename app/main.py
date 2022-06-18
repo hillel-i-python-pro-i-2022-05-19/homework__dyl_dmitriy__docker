d99c2c75ffc7b237e09756ec07e41724c05f0e7a
DylDmitriy <dyl.dmitriy@gmail.com>
2022-06-18 23:54:22 +0300

    doc 5

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
+import logging
+import pathlib
+from typing import Final
+
+import ulid
 from faker import Faker
 
 faker = Faker()
 
+ROOT_PATH: Final[pathlib.Path] = pathlib.Path(__file__).parents[1]
+OUTPUT_PATH: Final[pathlib.Path] = ROOT_PATH.joinpath('output')
+
 
 def generate_massege() -> str:
     return f"Hi!, {faker.unique.first_name()}"
+
+
+def generate_file():
+    temp_path = OUTPUT_PATH.joinpath(f'{ulid.new()}.txt')
+    temp_path.write_text(generate_message())
+
+    logging.info(f'File generated at path: "{temp_path}"')
